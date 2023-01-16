b9cd55162fadb943b21bf18f1d9ae0b961b5a7de
Alexander <klivets.alex@gmail.com>
2023-01-16 17:42:34 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const OPEN_BRACKETS = ['(', '[', '{', '|', '1', '3', '5', '7', '8'];
+  bracketsConfig.forEach((el) => {
+    el.reverse()
+  })
+const BRACKETS_PAIR = Object.fromEntries(bracketsConfig);
+let stack = [];
+for (let i = 0; i < str.length; i++) {
+  if (stack.includes(str[i]) && (stack.includes('|') || stack.includes('7') || stack.includes('8'))) {
+    stack.pop()
+  }
+  else if (OPEN_BRACKETS.includes(str[i])) {
+      stack.push(str[i])
+    }
+    else {
+      if (stack.length === 0) {
+        return false
+      }
+      if (BRACKETS_PAIR[str[i]] === stack[stack.length - 1]) {
+        stack.pop()
+      }
+      else {
+        return false
+      }
+    }
+}
+  return stack.length === 0;
 }
